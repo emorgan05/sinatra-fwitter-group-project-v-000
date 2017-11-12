ea2c109c9d796199a4c124b937f722e1aa822aba
Emily Morgan <emorgan05@gmail.com>
2017-11-12 23:43:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,38 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views, Proc.new { File.join(root, "../views/") }
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.create(params[:tweet])
+
+    redirect "/tweets/#{@tweet.id}"
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+
+    erb :'/tweets/edit'
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(params[:tweet])
+
+    redirect "/tweets/#{@tweet.id}"
+  end
+
+  get '/tweets/:id' do
+    erb :'/tweets/show'
   end
 
 end
